2dd171b2aba61dc8834f9370b228aa2b343a6777
Akash_Mohapatra <akash.zlatan@gmail.com>
2019-04-04 19:40:38 +0530

    cache-manager

---
+++ CachingManager/src/main/java/com/citizen/controller/HeadlinesController.java
@@ -0,0 +1,23 @@
+package com.citizen.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.citizen.model.NewsAPIResponse;
+import com.citizen.service.HeadlineService;
+
+@RestController
+public class HeadlinesController {
+	
+	@Autowired
+	private HeadlineService headlineService;
+	
+	@RequestMapping(value ="/HeadLines",method=RequestMethod.GET)
+	public NewsAPIResponse getNewResponse()
+	{
+		return headlineService.retrieveResults();
+	}
+
+}

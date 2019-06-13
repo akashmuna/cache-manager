b5090f0256e082ef2279696b90088cb0cbc20574
Akash_Mohapatra <akash.zlatan@gmail.com>
2019-06-13 12:23:16 +0530

    Tomcat Deployed War File

--- CachingManager/src/main/java/com/cache/CachingManagerApplication.java
+++ CachingManager/src/main/java/com/cache/CachingManagerApplication.java
@@ -3,9 +3,18 @@ package com.cache;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class CachingManagerApplication {
+@ComponentScan
+public class CachingManagerApplication extends SpringBootServletInitializer{
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	  {
+	    return application.sources(new Class[] { CachingManagerApplication.class });
+	  }
 
 	public static void main(String[] args) {
 		final SpringApplication app = new SpringApplication(CachingManagerApplication.class);

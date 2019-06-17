22485805adf075609bc37f61fb0ce194ed593a96
Akash_Mohapatra <akash.zlatan@gmail.com>
2019-06-17 20:17:52 +0530

    Using NetFlix OSS Zuul

--- CachingManager/src/main/java/com/cache/CachingManagerApplication.java
+++ CachingManager/src/main/java/com/cache/CachingManagerApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan
 public class CachingManagerApplication extends SpringBootServletInitializer{

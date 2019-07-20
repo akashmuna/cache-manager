217601a681a79c409310a84e1af7bddac63c592a
Akash_Mohapatra <akash.zlatan@gmail.com>
2019-07-20 15:17:23 +0530

    remove banner

--- CachingManagerAuthToken/src/main/java/com/cache/CachingManagerAuthToken.java
+++ CachingManagerAuthToken/src/main/java/com/cache/CachingManagerAuthToken.java
@@ -1,16 +1,21 @@
 package com.cache;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @ComponentScan
 @SpringBootApplication
 public class CachingManagerAuthToken {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CachingManagerAuthToken.class, args);
+		final SpringApplication app = new SpringApplication(CachingManagerAuthToken.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 }

568519195366bdb65f702afb124ce65af1738941
Akash_Mohapatra <akash.zlatan@gmail.com>
2019-07-20 15:18:05 +0530

    Eureka Server

--- GateWay/src/main/java/com/cache/gateway/GateWayApplication.java
+++ GateWay/src/main/java/com/cache/gateway/GateWayApplication.java
@@ -2,10 +2,9 @@ package com.cache.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @EnableEurekaServer
 @SpringBootApplication
 public class GateWayApplication {
